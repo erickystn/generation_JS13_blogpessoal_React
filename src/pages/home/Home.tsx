0b1e2617cb6e017af638464bc8b02e2518542f0a
erickystn <xkyx@live.com>
2026-04-07 19:28:24 -0300

    Deploy do Projeto Projeto Blog Pessoal

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,5 @@
+import ModalPostagem from "../../components/postagem/modalpostagem/ModalPostagem";
+
 function Home() {
     return (
         <>
@@ -12,11 +14,7 @@ function Home() {
                         </p>
 
                         <div className="flex justify-around gap-4">
-                            <div className='rounded text-white 
-                                            border-white border-solid border-2 py-2 px-4'
-                            >
-                                Nova Postagem
-                            </div>
+                            <ModalPostagem />
                         </div>
                     </div>
 
